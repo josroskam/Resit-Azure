89fa095f5a405eda6094fdc912ddee8578862ed8
josroskam <josroskam@hotmail.com>
2025-01-26 22:39:52 +0100

    ready to hand in

--- AzureWeatherJobs/FetchGeneratedImages/Program.cs
+++ AzureWeatherJobs/FetchGeneratedImages/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureFunctionsWebApplication()
     .ConfigureServices(services =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
